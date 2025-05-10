e3b30158e1988674d3ece9495ea3ab9093352039
ekidsorg <ekidsorg@gmail.com>
2025-05-10 16:08:07 +0200

    Update 003-genera-matrice-casuale.cpp

--- esercizi/003-genera-matrice-casuale.cpp
+++ esercizi/003-genera-matrice-casuale.cpp
@@ -3,12 +3,25 @@
   con numeri casuali interi compresi tra 0 e 30.
   Visualizzare la matrice per righe e colonne.
 */
+
 #include <iostream>
-using namespace std;    
+#include <cstdlib>      // including rand()
+#include <ctime>        // including time()
+using namespace std;
 
 int main()
-{
+{   
+    int numero_casuale = 0;
     
+    // generatore di numeri casuali
+    srand(time(0));
+
+    // esempio di generazione numero casuale
+    numero_casuale = rand() % 10; // tra 0 e 9
+    cout << "Esempio di numero casuale: " << numero_casuale << endl;
+    
+
+  
     // scrivi qui il tuo codice
     
     return 0;
