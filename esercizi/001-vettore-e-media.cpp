71476f345231e8f207f101c85c706a6bac84dde7
ekidsorg <ekidsorg@gmail.com>
2025-05-10 14:57:19 +0200

    Create 001-vettore-e-media.cpp

---
+++ esercizi/001-vettore-e-media.cpp
@@ -0,0 +1,26 @@
+/*
+Dato un vettore di 8 elementi di tipo interi 
+calcolare e stampare la somma e la media degli elementi.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int somma = 0;
+    double media = 0.0;
+    
+    // diachiara un vettore di 8 interi
+    // e inizializzalo con valori a tuo piacere
+
+    // calcola la somma ed assegnala alla variabile somma
+
+
+    // calcola la media ed assegnala alla variabile media
+
+
+    cout << "La somma dei valori contenuti nel vettore e': " << somma << endl;
+    cout << "La media dei valori contenuti nel vettore e': " << media << endl;
+
+    return 0;
+}
