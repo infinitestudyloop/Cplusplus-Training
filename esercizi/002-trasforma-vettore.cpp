b86631f9446afd97cffe90c5051b3666e4691629
ekidsorg <ekidsorg@gmail.com>
2025-05-10 15:31:29 +0200

    Create 002-trasforma-vettore.cpp

---
+++ esercizi/002-trasforma-vettore.cpp
@@ -0,0 +1,35 @@
+/*
+Crea un vettore di max 20 elementi di tipo carattere.
+Chiedi in input una parola.
+Chiedi in input un carattere che si vuole sostituire.
+Chiedi in input il carattere da usare per sostituirlo.
+Applicare la sostituzione
+Stampare la parola trasformata.
+*/
+#include <iostream>
+using namespace std;    
+
+int main()
+{
+    // Crea un vettore di max 20 elementi di tipo carattere.
+    char parola[20] {};
+    
+    // Chiedi in input una parola.
+    cout << "Inserisci una parola: " << endl;
+    cin >> parola;
+
+    // Chiedi in input un carattere che si vuole sostituire
+
+  
+    // Chiedi in input il carattere da usare per sostituirlo
+  
+  
+    // Applicare la sostituzione
+
+  
+    // Stampare la parola trasformata
+    cout << "\nHo capito bene che la tua parola e': " << parola << " ?" << endl;
+    cout << "Non occorre che tu risponda." << endl;
+  
+    return 0;
+}
