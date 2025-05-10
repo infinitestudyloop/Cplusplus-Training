ec1be5664ac0a494ee296e6ec651a1650b09beea
ekidsorg <ekidsorg@gmail.com>
2025-05-10 15:48:21 +0200

    Update 003-genera-matrice-casuale.cpp

--- esercizi/003-genera-matrice-casuale.cpp
+++ esercizi/003-genera-matrice-casuale.cpp
@@ -1,6 +1,15 @@
 /*
   Generare una matrice di 10 righe e 10 colonne 
-  con numeri casuali interi compresi tra 0 e 30
+  con numeri casuali interi compresi tra 0 e 30.
   Visualizzare la matrice per righe e colonne.
 */
+#include <iostream>
+using namespace std;    
 
+int main()
+{
+    
+    // scrivi qui il tuo codice
+    
+    return 0;
+}
